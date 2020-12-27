ef37f33d57c3108293c8fd2b4305a3b57e21b77a
kaajoj <kaajooj@gmail.com>
2020-12-27 19:49:00 +0100

    WalletOperations format

--- Services/WalletOperations.cs
+++ Services/WalletOperations.cs
@@ -3,34 +3,35 @@ using VSApi.Models;
 
 namespace VSApi.Services
 {
-  class WalletOperations
-  { 
-    public static Wallet CalculateSum(Wallet crypto) 
+    class WalletOperations
     {
-        crypto.Sum = Math.Round((Convert.ToDouble(crypto.Price) * Convert.ToDouble(crypto.Quantity)),2).ToString(); 
-        return crypto;
-    }
-    
-    public static string CalculateAlerts(Wallet crypto) {   
-        crypto.Change = Math.Round((((Convert.ToDouble(crypto.Price) / Convert.ToDouble(crypto.OldPrice)-1)*100)),2).ToString();
-        return crypto.Change;
-    }
-
-    public static void GetAlerts(Wallet crypto) 
-    {   
-        if(Convert.ToDouble(crypto.Change) < -Convert.ToDouble(crypto.AlertDown)) 
+        public static Wallet CalculateSum(Wallet crypto)
         {
-            string buyStr = "Price is below your alert(-" + crypto.AlertDown + "%)  -  buy  " + crypto.Name+"("+crypto.Symbol+")" + "";  
-            Emails emails = new Emails();
-            emails.prepareMessage(buyStr, crypto.Price, crypto.OldPrice, crypto.Change);
+            crypto.Sum = Math.Round((Convert.ToDouble(crypto.Price) * Convert.ToDouble(crypto.Quantity)), 2).ToString();
+            return crypto;
         }
-        if(Convert.ToDouble(crypto.Change) > Convert.ToDouble(crypto.AlertUp)) 
-        {
-            string sellStr = "Price is above your alert(" + crypto.AlertUp + "%)  -  sell  " + crypto.Name+"("+crypto.Symbol+")" + "";
-            Emails emails = new Emails();
-            emails.prepareMessage(sellStr, crypto.Price, crypto.OldPrice, crypto.Change);
-        }
-    }
 
-  }
+        public static string CalculateAlerts(Wallet crypto)
+        {
+            crypto.Change = Math.Round((((Convert.ToDouble(crypto.Price) / Convert.ToDouble(crypto.OldPrice) - 1) * 100)), 2).ToString();
+            return crypto.Change;
+        }
+
+        public static void GetAlerts(Wallet crypto)
+        {
+            if (Convert.ToDouble(crypto.Change) < -Convert.ToDouble(crypto.AlertDown))
+            {
+                string buyStr = "Price is below your alert(-" + crypto.AlertDown + "%)  -  buy  " + crypto.Name + "(" + crypto.Symbol + ")" + "";
+                Emails emails = new Emails();
+                emails.prepareMessage(buyStr, crypto.Price, crypto.OldPrice, crypto.Change);
+            }
+            if (Convert.ToDouble(crypto.Change) > Convert.ToDouble(crypto.AlertUp))
+            {
+                string sellStr = "Price is above your alert(" + crypto.AlertUp + "%)  -  sell  " + crypto.Name + "(" + crypto.Symbol + ")" + "";
+                Emails emails = new Emails();
+                emails.prepareMessage(sellStr, crypto.Price, crypto.OldPrice, crypto.Change);
+            }
+        }
+
+    }
 }
