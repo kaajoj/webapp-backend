2657c0fb046edaecadbafd0f027eb0e24fd1acc7
kaajoj <kaajooj@gmail.com>
2020-12-28 21:31:37 +0100

    Added interfaces IRepository and ICryptoRepository

---
+++ Interfaces/ICryptoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VSApi.Models;
+
+namespace VSApi.Interfaces
+{
+    public interface ICryptoRepository : IRepository<Crypto>
+    {
+    }
+}
---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VSApi.Interfaces
+{
+    public interface IRepository<TModel> where TModel : class
+    {
+        TModel Get(int id);
+        IEnumerable<TModel> GetAll();
+        void Add(TModel entity);
+        void Remove(TModel entity);
+    }
+}
