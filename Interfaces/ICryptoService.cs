83b3efd218ac87b044e4c67e3455ffb1965ba681
kaajoj <kaajooj@gmail.com>
2021-01-05 04:26:59 +0100

    Create CryptoService

---
+++ Interfaces/ICryptoService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VSApi.Models;
+
+namespace VSApi.Interfaces
+{
+    public interface ICryptoService
+    {
+        IEnumerable<Crypto> GetAll();
+    }
+}
---
+++ Services/CryptoService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VSApi.Interfaces;
+using VSApi.Models;
+
+namespace VSApi.Services
+{
+    public class CryptoService : ICryptoService
+    {
+        private readonly ICryptoRepository _cryptoRepository;
+
+        public CryptoService(ICryptoRepository cryptoRepository)
+        {
+            _cryptoRepository = cryptoRepository;
+        }
+        public IEnumerable<Crypto> GetAll()
+        {
+            return _cryptoRepository.GetAll().OrderBy(c => c.Rank);
+        }
+    }
+}
